6f0d5d217190c9039c0bd3d7feae330ae304e4fb
ReinekeWF <benjamin.sprenger@gmail.com>
2019-07-28 13:56:14 +0200

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 host = 'irc.chat.twitch.tv'
 port = 6667
 nick = 'kleinerfuchsbot'
-oauth = 'oauth:ry3j3g9bjqeuha80vthdseidwl7or'
+oauth = 'oauth:'
 
 # Favorisirte Channel
 ChannelList = ['artimus83',
